131259affa5598bf88c205feb53d21708075ab0a
Ismael Teixeira da Silva <ismaelteixeiradas1@hotmail.com>
2024-11-23 22:41:08 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
+  headers: {
+    ...(localStorage.getItem('token') && {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    }),
+  },
 })
 
 export default api
