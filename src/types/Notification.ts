ab7c38d1aa2f197185e1d916bd8de40d1e9c352b
bkuste2 <igorribeiro632@gmail.com>
2024-12-01 14:13:02 -0300

    :rocket: feat: Add Notification type and interface

---
+++ src/types/Notification.ts
@@ -0,0 +1,23 @@
+import { User } from "./User";
+
+export enum NotificationStatus {
+  READ = "READ",
+  UNREAD = "UNREAD"
+}
+
+export enum NotificationType {
+  ORDER = "ORDER",
+  MESSAGE = "MESSAGE"
+}
+
+export interface Notification {
+  id?: string;
+  title: string;
+  message: string;
+  status: NotificationStatus;
+  type: NotificationType;
+  createdAt?: string;
+  updatedAt?: string;
+  userId: string;
+  user: User;
+}
